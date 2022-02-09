c81b0b2591831e03512e9d0d825f5ce41f9fbd65
Romain Cambonie <romain.cambonie@gmail.com>
2022-02-09 14:05:39 +0100

    Clevercloud setup workflow

--- src/application/hello.tsx
+++ src/application/hello.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export const Hello = () => {
+export const hello = () => {
   return <div>Hello world !</div>;
 };
--- src/application/server.ts
+++ src/application/server.ts
@@ -1,13 +1,13 @@
 import express, { Express } from 'express';
 import ReactDOMServer from 'react-dom/server';
-import { Hello } from './Hello';
+import { hello } from './hello';
 
 const PORT: number = parseInt(process.env.PORT ?? '3000');
 
 const app: Express = express();
 
 app.get('/ping', (_: express.Request, response: express.Response): void => {
-  response.send(ReactDOMServer.renderToString(Hello()));
+  response.send(ReactDOMServer.renderToString(hello()));
 });
 
 app.listen(PORT, (): void => {
