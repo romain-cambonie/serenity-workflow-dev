967d946a5a22a0fe13a5289709ab2d48d30a19d9
Romain Cambonie <romain.cambonie@gmail.com>
2022-05-21 22:14:03 +0200

    prepared test project

---
+++ .eslint/eslint-test.rules.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'max-lines-per-function': [
+    'error',
+    {
+      max: 32,
+      skipBlankLines: true,
+      skipComments: true
+    }
+  ],
+  'max-nested-callbacks': ['error', 4],
+  'max-statements': 'off'
+};
